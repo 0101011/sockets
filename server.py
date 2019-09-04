d221b9c6e4de60c30f20bf81a6e3ea587fe263d2
0101011 <andrew.stepin@gmail.com>
2019-09-04 11:36:50 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))  # Define end point for communication
+s.listen()
+
+while True:
+	clientsocket, address = s.accept()
+	print(f'Connection from {address} has been established.')
+	clientsocket.send(bytes('Welcome to the server', 'utf-8'))
